b57e4d85dbd0f3ec786026f4cea03a2f55c5149f
ReisenLOL <157349179+ReisenLOL@users.noreply.github.com>
2025-05-02 23:32:05 -0700

    add spell location

--- Assets/Scripts/Spells/Spell.cs
+++ Assets/Scripts/Spells/Spell.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 
 public class Spell : MonoBehaviour
 {
+    public SpellListUI spellListUI;
     public Sprite icon;
     public string spellName;
     public List<KeyCode> inputCode = new();
@@ -12,7 +13,13 @@ public class Spell : MonoBehaviour
     public bool playerIsCasting;
     private int currentIndex = 0;
     public bool thisSpellIsCasting;
+    public Vector2 spellLocation;
+    public bool hasChosenLocation;
 
+    protected virtual void Start()
+    {
+        spellListUI = FindFirstObjectByType<SpellListUI>();
+    }
     protected virtual void Update()
     {
         if (playerIsCasting)
@@ -36,12 +43,26 @@ public class Spell : MonoBehaviour
         }
         if (thisSpellIsCasting)
         {
-            castingTimeElapsed += Time.deltaTime;
-            if (castingTimeElapsed > castingTime)
+            if (!hasChosenLocation && Input.GetMouseButton(0))
             {
-                CastSpell();
-                castingTimeElapsed = 0;
+                Vector3 mousePos = Input.mousePosition;
+                mousePos.z = Camera.main.nearClipPlane + 10;
+                spellLocation = Camera.main.ScreenToWorldPoint(mousePos);
+                hasChosenLocation = true;
+            }
+            if (!hasChosenLocation && Input.GetKeyUp(KeyCode.LeftControl))
+            {
                 thisSpellIsCasting = false;
+            }
+            if (hasChosenLocation)
+            {
+                castingTimeElapsed += Time.deltaTime;
+                if (castingTimeElapsed > castingTime)
+                {
+                    CastSpell();
+                    castingTimeElapsed = 0;
+                    thisSpellIsCasting = false;
+                }
             }
         }
     }
@@ -51,6 +72,6 @@ public class Spell : MonoBehaviour
     }
     protected virtual void CastSpell()
     {
-        Debug.Log("Casted Spell: " + spellName);
+        Debug.Log("Casted Spell: " + spellName + " at " + spellLocation);
     }
 }
