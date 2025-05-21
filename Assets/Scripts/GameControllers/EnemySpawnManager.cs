2704809c5141e55918dc5a8e39de79c9fec0ffb2
ReisenLOL <157349179+ReisenLOL@users.noreply.github.com>
2025-05-20 23:05:22 -0700

    destroying windows NOW

--- Assets/Scripts/GameControllers/EnemySpawnManager.cs
+++ Assets/Scripts/GameControllers/EnemySpawnManager.cs
@@ -9,10 +9,22 @@ public class EnemySpawnManager : MonoBehaviour
         public Transform[] validSpawnLocations; //placeholder, this will be areas it can spawn in.
     }
     //boring surround spawning, will replace with enemy groups and patrols later.
+    public EnemySpawnGroup[] spawnGroups;
     public float spawnRate;
     public float spawnTime;
     public Unit enemyToSpawn;
     public Transform[] spawnPoints;
+
+    private void Start()
+    {
+        for (int i = 0; i < spawnGroups.Length; i++)
+        {
+            for (int j = 0; j < spawnGroups[i].validSpawnLocations.Length; j++)
+            {
+                SpawnUnitsFromGroup(spawnGroups[i], spawnGroups[i].validSpawnLocations[j]);
+            }
+        }
+    }
 
     private void Update()
     {
@@ -31,12 +43,12 @@ public class EnemySpawnManager : MonoBehaviour
             newEnemy.transform.position = locationToSpawn;
         }
     }
-    public void SpawnUnitsFromGroup(EnemySpawnGroup spawnGroup)
+    public void SpawnUnitsFromGroup(EnemySpawnGroup spawnGroup, Transform spawnLocation)
     {
         for (int i = 0; i < spawnGroup.enemiesToSpawn.Length; i++)
         {
             Unit newEnemy = Instantiate(spawnGroup.enemiesToSpawn[i]);
-            newEnemy.transform.position = spawnGroup.validSpawnLocations[i].position;
+            newEnemy.transform.position = spawnLocation.position;
         }
     }
 }
